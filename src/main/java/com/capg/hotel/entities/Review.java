ac8d854ff674b1dff2b14a191ff976e1b99cc3c2
Ishank <deshwalishank@gmail.com>
2026-04-29 11:20:56 +0530

    bean validation

--- src/main/java/com/capg/hotel/entities/Review.java
+++ src/main/java/com/capg/hotel/entities/Review.java
@@ -2,6 +2,13 @@ package com.capg.hotel.entities;
 
 import java.time.LocalDate;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
@@ -14,16 +21,24 @@ public class Review {
     private Integer reviewId;
 
     @Column(name = "rating")
+    @NotNull(message = "Rating cannot be null")
+    @Min(value = 1, message = "Rating must be at least 1")
+    @Max(value = 5, message = "Rating cannot exceed 5")
     private Integer rating;
 
     @Column(name = "comment")
+    @NotBlank(message = "Comment cannot be empty")
+    @Size(min = 5, max = 300, message = "Comment must be between 5 and 300 characters")
     private String comment;
 
     @Column(name = "review_date")
+    @NotNull(message = "Review date cannot be null")
+    @PastOrPresent(message = "Review date cannot be in the future")
     private LocalDate reviewDate;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "reservation_id")
+    @NotNull(message = "Reservation must be associated with review")
     @JsonIgnoreProperties({"reviews", "hibernateLazyInitializer", "handler"})
     private Reservation reservation;
 
